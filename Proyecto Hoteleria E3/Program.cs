9bb6f86f0632311d5a161f28afdd4d0adcb0afa1
B4stian07 <sebas.davila2706@gmail.com>
2025-02-22 14:08:45 -0600

    Inicia proyecto

---
+++ Proyecto Hoteleria E3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Hoteleria_E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOla mundo");
+        }
+    }
+}
