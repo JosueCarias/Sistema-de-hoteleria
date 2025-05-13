abed38af3499da3c649e6e2fe5eb048af74d8a00
JosueCarias <josuecarias120401@gmail.com>
2025-05-12 23:21:42 -0600

    PDF reporte

--- API/hoteleria/obj/Debug/net9.0/hoteleria.AssemblyInfo.cs
+++ API/hoteleria/obj/Debug/net9.0/hoteleria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hoteleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b945ed7a34443bb5622d02a5c33d3d9f1db64b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd91f723dace718bfdeb0b956a9a943b4c885196")]
 [assembly: System.Reflection.AssemblyProductAttribute("hoteleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hoteleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
